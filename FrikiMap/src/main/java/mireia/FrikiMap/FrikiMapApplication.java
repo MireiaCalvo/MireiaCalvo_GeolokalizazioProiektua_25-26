9c9ddeaf505e9bf214daaf73dbcb4f75e8b8b8a4
RoseShooter <calvomojon.mireia@gmail.com>
2025-12-20 11:32:01 +0100

    proiektuarentzako api-a

---
+++ FrikiMap/src/main/java/mireia/FrikiMap/FrikiMapApplication.java
@@ -0,0 +1,13 @@
+package mireia.FrikiMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrikiMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrikiMapApplication.class, args);
+	}
+
+}
---
+++ FrikiMap/src/test/java/mireia/FrikiMap/FrikiMapApplicationTests.java
@@ -0,0 +1,13 @@
+package mireia.FrikiMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrikiMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
